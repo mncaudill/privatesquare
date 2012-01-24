0dbe9b8b1fcf71fa28ae1648c77da21e9cb7fc0a
Nolan Caudill <nolan@nolancaudill.com>
2012-01-23 22:02:04 -0800

    Show recent checkins on homepage

--- www/index.php
+++ www/index.php
@@ -14,8 +14,24 @@
 	$status_map = privatesquare_checkins_status_map();
 	$broadcast_map = foursquare_checkins_broadcast_map();
 
+	$more = array(
+		'page' => 1,
+		'per_page' => 5,
+		'spill' => 0,
+	);
+	$checkins = privatesquare_checkins_for_user($GLOBALS['cfg']['user'], $more);
+
+	$cleaned_checkins = array();
+	
+	if ($checkins['ok']) {
+		foreach ($checkins['rows'] as $checkin) {
+			$cleaned_checkins[] = $checkin['venue'];
+		}
+	}
+
 	$GLOBALS['smarty']->assign_by_ref("status_map", $status_map);
 	$GLOBALS['smarty']->assign_by_ref("broadcast_map", $broadcast_map);
+	$GLOBALS['smarty']->assign_by_ref("user_checkins", $cleaned_checkins);
 
 	$GLOBALS['smarty']->display("page_index.txt");
 	exit();
