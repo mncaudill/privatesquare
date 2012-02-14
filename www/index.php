4cef30258dc5ba155c9a3ab93a16eb5d98c4740d
Nolan Caudill <nolan@nolancaudill.com>
2012-02-13 18:40:05 -0800

    Don't need my changes anymore!

--- www/index.php
+++ www/index.php
@@ -19,19 +19,9 @@
 		'per_page' => 5,
 		'spill' => 0,
 	);
-	$checkins = privatesquare_checkins_for_user($GLOBALS['cfg']['user'], $more);
-
-	$cleaned_checkins = array();
-	
-	if ($checkins['ok']) {
-		foreach ($checkins['rows'] as $checkin) {
-			$cleaned_checkins[] = $checkin['venue'];
-		}
-	}
 
 	$GLOBALS['smarty']->assign_by_ref("status_map", $status_map);
 	$GLOBALS['smarty']->assign_by_ref("broadcast_map", $broadcast_map);
-	$GLOBALS['smarty']->assign_by_ref("user_checkins", $cleaned_checkins);
 
 	$GLOBALS['smarty']->display("page_index.txt");
 	exit();
